98eda376749e88f8f7bffcc363d9b3f2346fd680
Justin Singh <kbardosh@gmail.com>
2025-02-11 10:45:50 -0800

    updated services section

--- src/app/_components/services-section.tsx
+++ src/app/_components/services-section.tsx
@@ -16,144 +16,14 @@ const serviceSRC =
 
 const treatments: TreatmentCardProps["treatment"][] = [
   {
-    title: "Consultation (In Person)",
-    category: "Consultations",
-    price: 0,
-    imageUrl: serviceSRC,
-
-    descriptions: [
-      {
-        heading: "In-Person Consultation",
-        text: "Consultations are an essential part of our process, allowing us to assess your needs and discuss your goals in person. During the consultation, our providers will evaluate your skin, discuss your aesthetic concerns, and recommend the best treatments tailored to your needs.",
-      },
-      {
-        heading: "What to Expect During Your Consultation",
-        text: "You’ll have the opportunity to ask questions, learn about the various treatment options available, and gain a clear understanding of what to expect. This ensures that you make informed decisions about your aesthetic journey.",
-      },
-    ],
-  },
-  {
-    title: "Good Faith Exam",
-    category: "Consultations",
-    price: 0,
-    imageUrl: serviceSRC,
-
-    descriptions: [
-      {
-        heading: "Virtual Exam with Wendy Delgado, PA",
-        text: "A virtual exam done by Wendy Delgado, PA, ensures that you are cleared for cosmetic procedures. Wendy and Karina work closely together to ensure that each patient is healthy and suitable for treatment.",
-      },
-      {
-        heading: "Customized Treatment Plan",
-        text: "Wendy and Karina collaborate to provide the perfect treatment plan tailored to your needs, ensuring the best results for your cosmetic procedure.",
-      },
-    ],
-  },
-  {
-    title: "Botulinum Toxin Injections: New Patient",
+    title: "Botulinum Toxin Injections",
     category: "Injectables",
     price: 0,
-    imageUrl: serviceSRC,
+    imageUrl: 'https://g19i0ofs1h.ufs.sh/f/6qEAXnDYaqZeb5fPMB6LfVokWXU2rFQOwaT531CYvjh8yMNc',
     descriptions: [
       {
-        heading: "What is Botox®, Dysport®, Xeomin®, aka, Neuromodulators?",
-        text: "These products relax the muscles of the face and neck to diminish and prevent wrinkles. Lines/wrinkles that are present on the face without expression are called static lines. The use of these products can prevent such lines from ever forming.",
-      },
-      {
-        heading: "How Long Do Results Last?",
-        text: "Results typically last about 3 months. Oftentimes, the skin will look better for longer than 3 months. However, we recommend treatment every 3 months so that old wrinkles don’t have time to reform and new ones don’t appear.",
-      },
-      {
-        heading: "Other Uses:",
-        text: "These products can also be used for hyperhidrosis (excessive sweating). The injections are placed into the armpit. Botox® or Dysport® for sweating are very effective.",
-      },
-      {
-        heading: "Botox® vs. Dysport®:",
-        text: "Botox® and Dysport® are dosed differently, but the same number of injection sites are needed per treatment and the final cost is identical in most cases. Dysport® generally starts working faster than Botox®. Both are made of the same protein by different companies and are highly effective.",
-      },
-    ],
-  },
-  {
-    title: "Botulinum Toxin: Return Patient",
-    category: "Injectables",
-    price: 0,
-    imageUrl: serviceSRC,
-    descriptions: [
-      {
-        heading: "What is Botox®, Dysport®, Xeomin®, aka, Neuromodulators?",
-        text: "These products relax the muscles of the face and neck to diminish and prevent wrinkles. Lines or wrinkles that are present on the face without expression are called static lines. The use of these products can prevent such lines from ever forming.",
-      },
-      {
-        heading: "How Long Do Results Last?",
-        text: "Results typically last about 3 months. Oftentimes, the skin will look better for longer than 3 months. However, we recommend treatment every 3 months so that old wrinkles don’t have time to reform and new ones don’t appear.",
-      },
-      {
-        heading: "Other Uses",
-        text: "These products can also be used for hyperhidrosis (excessive sweating). The injections are placed into the armpit. Botox® or Dysport® for sweating are very effective.",
-      },
-      {
-        heading: "Botox® vs. Dysport®",
-        text: "Botox® and Dysport® are dosed differently, but the same number of injection sites are needed per treatment and the final cost is identical in most cases. Dysport® generally starts working faster than Botox®. Both are made of the same protein by different companies and are highly effective.",
-      },
-    ],
-  },
-  {
-    title: "Facial Filler",
-    category: "Injectables",
-    price: 0,
-    imageUrl: serviceSRC,
-    descriptions: [
-      {
-        heading: "Plan Ahead",
-        text: "Please plan your treatment to allow for several days or more of potential swelling, tenderness, and bruising.",
-      },
-      {
-        heading: "Postpone If You Have Certain Conditions",
-        text: "If you have an active cold sore/fever blister, canker sore, or shingles, you will need to postpone treatment. If you are prone to these conditions, contact us 1 week before treatment for recommendations.",
-      },
-      {
-        heading: "Dental Work Restrictions",
-        text: "No Vycross® fillers (Voluma®, Vollure®, Volbella®) should be done within 2 weeks of any dental work or dental cleaning.",
-      },
-      {
-        heading: "Inform Us of New Medications",
-        text: "If you’ve started any antibiotics or other medications since your initial visit, please let us know.",
-      },
-      {
-        heading: "Avoid Certain Medications and Supplements",
-        text: "Avoid Advil®, Ibuprofen, Aleve®, Aspirin, Motrin®, Vitamin E, fish oil, St. John's Wort, and other non-steroidal anti-inflammatory products 5 days prior to treatment to decrease your risk of bruising. Do not stop prescribed medications without consulting your physician.",
-      },
-      {
-        heading: "Medical History Disclosure",
-        text: "Inform your Master Provider of all medical conditions, medications you are taking, skin disorders, allergies, history of anaphylaxis, and any other medical problems.",
-      },
-      {
-        heading: "Avoid Alcohol",
-        text: "Avoid drinking alcoholic beverages 48 hours before treatment to reduce the risk of bruising.",
-      },
-      {
-        heading: "Consider Arnica for Bruising",
-        text: "To reduce the risk of bruising, consider taking over-the-counter Arnica supplements if not contraindicated. Start taking oral Arnica 7-10 days before treatment as directed on the bottle.",
-      },
-      {
-        heading: "Tear Trough Filler Precaution",
-        text: "If you are having dermal filler placed in your tear trough, take an antihistamine (Claritin®, Zyrtec®, or Loratadine®) for 3 days prior to treatment to help reduce post-treatment swelling.",
-      },
-      {
-        heading: "Pregnancy and Breastfeeding",
-        text: "Women who are pregnant or breastfeeding cannot be treated.",
-      },
-      {
-        heading: "Chemotherapy or Radiation Restrictions",
-        text: "Patients actively undergoing chemotherapy or radiation treatment cannot be treated. Patients must be in remission for 12 months to be treated.",
-      },
-      {
-        heading: "Accutane® Use",
-        text: "If you have recently used the medication Accutane®, it is recommended to postpone treatment until at least six months after stopping the medication.",
-      },
-      {
-        heading: "Tylenol® Use",
-        text: "Tylenol® will not increase the chance of bruising and is preferred for the week prior to treatment. To help with possible discomfort, you may take acetaminophen (Tylenol®) one hour before your appointment. Do not exceed 3000mg of acetaminophen in a 24-hour period.",
+        heading: "",
+        text: "Botulinum toxins are neuromodulators that relax the muscles of the face and neck, to diminish and prevent wrinkles and fine lines.",
       },
     ],
   },
@@ -161,69 +31,82 @@ const treatments: TreatmentCardProps["treatment"][] = [
     title: "Lip Filler",
     category: "Injectables",
     price: 0,
-    imageUrl: serviceSRC,
+    imageUrl: "https://g19i0ofs1h.ufs.sh/f/6qEAXnDYaqZeaV8z6HEg4QmlHMy1789ZfiedpR6E0NIhjAkF",
     descriptions: [
       {
-        heading: "What are Lip Fillers?",
-        text: "Lip fillers, such as the Juvederm® and Restylane® brands, are made from hyaluronic acid, a naturally occurring substance in our bodies. They can replace or add volume in our lips for plumper, fuller, or more defined lips.",
+        heading: "",
+        text: "Lip filler is made from hyaluronic acid, a naturally occurring substance in our bodies. It can replace and add volume in our lips for plumper, fuller, and more defined lips.",
       },
-      {
-        heading: "Aging and Lip Changes",
-        text: "As people age, our lips age with us, gradually becoming smaller and developing more vertical lines.",
-      },
+  
     ],
   },
   {
-    title: "Lip Filler Touch Up",
+    title: "Sculptra",
     category: "Injectables",
     price: 0,
-    imageUrl: serviceSRC,
+    imageUrl: "https://g19i0ofs1h.ufs.sh/f/6qEAXnDYaqZeYOBJaYLvihlY3zCy5pregGtJx1fnLmKuMcwQ",
     descriptions: [
       {
-        heading: "Lip Filler Touch Up",
-        text: "A Lip Filler touch-up is designed to enhance or maintain your previous lip filler treatment, ensuring optimal volume, symmetry, and definition.",
+        heading: "",
+        text: "Sculptra is made of poly-L-lactic acid (PLLA), that stimulates collagen and elastin production in the skin over the course of several months, resulting in improved skin tone and texture, enhanced facial volume, and increased skin elasticity.",
       },
+  
     ],
   },
   {
-    title: "Microneedling",
+    title: "Facial Filler",
     category: "Injectables",
     price: 0,
-    imageUrl: serviceSRC,
-
+    imageUrl: "https://g19i0ofs1h.ufs.sh/f/6qEAXnDYaqZen5bvsLKk7oYZFe5IQxVL148W3Bqhp9NCATya",
     descriptions: [
       {
-        heading: "What is Microneedling?",
-        text: "Microneedling is a procedure aimed at stimulating the body’s own collagen production to reduce the appearance of fine lines, pores, wrinkles, stretch marks, skin laxity, and scarring. Topical lidocaine, along with the high-speed automated needling motion, minimizes pain and discomfort.",
+        heading: "",
+        text: "Filler that is strategically placed in order to create facial balancing, is aimed at enhancing the overall harmony and balance of facial features. This is a achieved by adjusting the proportions and volume of various facial elements to create a more aesthetically pleasing appearance.",
       },
-      {
-        heading: "Why Choose Medical-Grade Microneedling?",
-        text: "Our medical-grade microneedling is far more efficient than any at-home needling/roller or manual procedure. At-home rollers cannot generate the same depth of microholes in the skin and will not yield the same consistent results.",
-      },
+  
     ],
   },
   {
-    title: "Sculptra Injections",
+    title: "Skincare",
     category: "Injectables",
     price: 0,
-    imageUrl: serviceSRC,
+    imageUrl: "https://g19i0ofs1h.ufs.sh/f/6qEAXnDYaqZeZ1j4F3nhBMKVoDZuNFqmxwRL9JUvz6ibHfCk",
     descriptions: [
       {
-        heading: "What is Sculptra®?",
-        text: "Sculptra® is an injectable solution that triggers the production of collagen, which helps rebuild areas affected by age-related volume loss and sagging. After your Sculptra® treatment at Humminglow Aesthetics Lounge, collagen production gradually increases, making the volume-enhancing results of the treatment apparent over time.",
+        heading: "",
+        text: "Individualized medical grade skincare products for your specific skin needs, that are can improve skin tone, texture, radiance, and reduce the appearance of wrinkles and blemishes. ",
+      },
+  
+    ],
+  },
+  {
+    title: "Laser Treatments",
+    category: "Injectables",
+    price: 0,
+    imageUrl: "https://g19i0ofs1h.ufs.sh/f/6qEAXnDYaqZefF55sBrcFaY3vTnmpGShI2ugHAMdoECjXKz7",
+    descriptions: [
+      {
+        heading: "",
+        text: "The Aerolase laser is a non-invasive laser treatment that offers skin rejuvenation benefits such as: reducing wrinkles, fine lines, and age spots, improving skin texture and tone, and tightening and lifting the skin.",
+      },
+  
+    ],
+  },
+  {
+    title: "SkinPen Microneedling",
+    category: "Injectables",
+    price: 0,
+    imageUrl: "https://g19i0ofs1h.ufs.sh/f/6qEAXnDYaqZe4MxEZhAQdzvb3JqrHwZjelyO7hic9a5L1T0f",
+    descriptions: [
+      {
+        heading: "",
+        text: "The first FDA-cleared microneedling device SkinPen, targets facial acne scars, face and neck wrinkles, and helps stimulate collagen production, for smoother, more radiant, younger-looking skin.",
       },
       {
-        heading: "Benefits of Sculptra®",
-        text: "Sculptra® can help improve the appearance of sagging jowls, hollow temples, and diminished cheeks, creating a fuller, more youthful appearance.",
+        heading: "",
+        text: "Can be used in addition with platelet-rich plasma (PRP), which contains growth factors that promotes wound healing and collagen synthesis.",
       },
-      {
-        heading: "Longevity of Results",
-        text: "Sculptra® has longer-lasting results compared to some other cosmetic dermal fillers like Restylane® and Juvederm®. The results can last for up to two years.",
-      },
-      {
-        heading: "Treatment Plan",
-        text: "In most patients, a total of 6 vials are needed, split between 3 treatments 6 weeks apart.",
-      },
+  
     ],
   },
 ];
