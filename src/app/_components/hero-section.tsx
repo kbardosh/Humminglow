7bece8f5b3ea4b77b710e6ba79642d7ebf1c5f63
Justin Singh <humminglowdev@gmail.com>
2025-06-25 06:42:08 -0700

    hero is now looking better at smaller screens

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -3,13 +3,13 @@ import { Hero } from "./hero";
 export const HeroSection = () => {
   return (
     <div
-      className="flex"
+      className="py-56"
       style={{
         backgroundImage: `url(https://utfs.io/f/0h3ZZPNH4XVb3J92HWG1bOE2oWJBx4tLl0Iy6kcguHpzseZV)`,
         height: "calc(100vh - 6.5rem)",
       }}
     >
-      <header className="relative w-full pt-56">
+      <header className="relative w-full">
         <Hero />
       </header>
     </div>
